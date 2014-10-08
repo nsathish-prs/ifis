a15faca29c497a2d8140f12d4fb777c8b939ee78
intern <intern@ifiss-MacBook.local>
2014-10-08 12:41:16 +0800

    Initial Commit

---
+++ tradeportal/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  tradeportal
+//
+//  Created by intern on 8/10/14.
+//  Copyright (c) 2014 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
